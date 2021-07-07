e126cef64948b344c795aa66f84aec5a5c6d7f2b
xuhao <13951229744@163.com>
2021-07-07 16:41:10 +0800

    Bee Go初始框架

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	_ "goBlog/routers"
+
+	"github.com/astaxie/beego"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
-}
-
 func main() {
-    http.HandleFunc("/", handlerFunc)
-    http.ListenAndServe(":3000", nil)
+	beego.Run()
 }
